4cbedf942636dc21150985f049f7dae83f659262
Rodrigo <roodrigoribeiro@hotmail.com>
2025-09-24 18:38:29 -0300

    implementando a função de adicionar o jogo na biblioteca

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,36 @@ import { useState } from "react"
 export default function App() {
   const [title, setTitle] = useState("")
   const [cover, setCover] = useState("")
+  
 
   const handleSubmit = (ev) => {
-    ev.prevenDault()
+    ev.preventDefault()
+    console.log({ title, cover})
+    setTitle("")
+    setCover("")
   }
   return (
     <div id="app">
       <h1>Biblioteca de Jogos</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="title">Titulo:</label>
-          <input type="text"
-           name="title"
+          <input 
+            type="text"
+            name="title"
             id="title"
-            
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             />
         </div>
         <div>
           <label htmlFor="cover">Capa:</label>
-          <input type="text"
-           name="cover"
+          <input 
+            type="text"
+            name="cover"
             id="cover"
-            
+            value={cover}
+            onChange={(e) => setCover (e.target.value)}
             />
         </div>
         <button type="submit">Adicionar à biblioteca</button>
