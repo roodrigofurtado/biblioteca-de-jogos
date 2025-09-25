5ac30ef3f5cce44723c021c6a68a2b9a24017e94
Rodrigo <roodrigoribeiro@hotmail.com>
2025-09-25 15:51:38 -0300

    adicionando alteração

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,29 @@
-import { useState } from "react"
+import NewGameForm from "./components/NewGameForm"
 import Game from "./components/Game"
+import useGameCollection from "./hooks/useGameCollection"
 
- function App() {
-  const [games, setGames] = useState(() => {
-    const storedGames = localStorage.getItem("obc-game-lib")
-    if (!storedGames) return []
-    return JSON.parse(storedGames)
-  })
+function App() {
+  const { games, addGame, removeGame } = useGameCollection()
 
-  const addGame = ({ title, cover}) => {
-    const id = Math.floor(Math.random() * 1000000)
-    const game = { id, title, cover}
-    setGames(state => {
-      const newState = [...state, game]
-      localStorage.setItem("obc-game-lib", JSON.stringify(newState))
-      return newState
-    })
-  }
-
-    const removeGame = (id) => {
-    setGames(state => {
-      const newState = state.filter(game => game.id !== id)
-      localStorage.setItem("obc-game-lib", JSON.stringify(newState))
-      return newState
-    })
-  }
   return (
-    <div id="app">
+    <div className="app">
       <h1>Biblioteca de Jogos</h1>
       <NewGameForm addGame={addGame} />
       <div className="games">
-        {gaems.map((game) => (
-          <Game 
+        {games.length > 0 ? 
+         games.map((game) => (
+          <Game
             key={game.id}
             title={game.title}
             cover={game.cover}
             onRemove={() => removeGame(game.id)}
-          />
-        ))}
+          />))
+          : ( 
+            <h2>Parece que ainda não tem nada aqui. Tente adicionar alguns jogos!</h2>
+        )}
       </div>
     </div>
   )
 }
+
 export default App
